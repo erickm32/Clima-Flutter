c7e6a3ea27fc6c3d35566595e3f4e728257032d6
Érick Moreira <erick@sementesrastreadas.com.br>
2020-11-09 21:47:11 -0300

    Getting location data and opening a new screen to display it

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,11 @@
+import 'package:clima/screens/location_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'location_screen.dart';
+
+const API_KEY = 'd36f18a6d34439b4684fcf2166660e12';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,20 +13,33 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  void getLocation() async {
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high);
-    print(position);
-  }
-
   @override
   void initState() {
     super.initState();
-    getLocation();
+    getLocationData();
+  }
+
+  void getLocationData() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    HttpHelper httpHelper = HttpHelper(
+        'https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=${API_KEY}&units=metric');
+    var data = await httpHelper.getAPIData();
+
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen(data);
+    }));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: SpinKitCircle(
+          color: Colors.white,
+        ),
+      ),
+    );
   }
 }
